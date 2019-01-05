8ba1eb4311161aabf9d3f3f8e855b88bc6cd3e88
Chirag Dugar <c.dugar011@gmail.com>
2019-01-05 06:19:47 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res) =>{
+  // res.send('<h1>Hello to my first express app</h1>');
+  res.send({
+    name: 'CD',
+    likes: ['a','b','c']
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    paget: "Hey CD",
+    thisyear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000,()=>{
+  console.log('The server is up boi');
+});
